6a7e5bea426c01395a060139ad54d8ae890c2c39
TE23B-Nathanailidis-Kevin <kevin.nathanailidis@elev.ga.ntig.se>
2024-09-17 13:19:59 +0200

    first commit

---
+++ Slagsmålspelet/Program.cs
@@ -0,0 +1,16 @@
+﻿int HumanHP = 100;
+int AlienHP = 100;
+
+string humanname = "HUMAN";
+string alienname = "ALIEN";
+
+Random genetertor = new Random();
+
+while (HumanHP > 0 && AlienHP > 0 )
+
+{
+    Console.WriteLine("\n ------))New Round((------");
+    Console.WriteLine($"{humanname}: {HumanHP}   {alienname}: {AlienHP})\n");
+
+    int 
+} 
